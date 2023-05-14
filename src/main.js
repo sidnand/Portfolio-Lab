5d1444231cd55927a22cf8a8729439a95cc4844a
Siddharth Nand <snand233@gmail.com>
2023-05-14 13:40:12 -0700

    preset functionality

--- src/main.js
+++ src/main.js
@@ -1,46 +1,205 @@
 let tableSharpes = document.getElementById('table_sharpes')
 let tableSignifs = document.getElementById('table_signifs')
 
-function loadClick(x) {
+let selectedPreset = ""
+
+let handleFormSubmit = () => {
+    // get the form data
+    let form = document.getElementById('form')
+    let file = document.getElementById('file').files[0]
+    let delimType = form.elements['delim'].value
+    let dateFormat = form.elements['date-format'].value
+    let riskFactor = form.elements['risk-factor'].value
+    let riskFree = form.elements['risk-free'].value
+    let timeHorizons = form.elements['time-horizons'].value
+    let gammas = form.elements['gammas'].value
+    let dateRangeStart = form.elements['date-range-start'].value
+    let dateRangeEnd = form.elements['date-range-end'].value
+    let models = form.elements['model']
+    let checkedModels = []
+
+    for (let i = 0; i < models.length; i++) {
+        if (models[i].checked) {
+            checkedModels.push(models[i].value)
+        }
+    }
+
+    // read the file which is a csv and save it as a string
+    let reader = new FileReader()
+    reader.readAsText(file, "UTF-8")
+    reader.onload = function (evt) {
+        fileData = evt.target.result
+
+        let data = {
+            'fileData': fileData,
+            'selectedPreset': selectedPreset,
+            'delimType': delimType,
+            'dateFormat': dateFormat,
+            'riskFactor': riskFactor,
+            'riskFree': riskFree,
+            'timeHorizons': timeHorizons,
+            'gammas': gammas,
+            'dateRangeStart': dateRangeStart,
+            'dateRangeEnd': dateRangeEnd,
+            'models': checkedModels,
+        }
+
+        run(data)
+    }
+}
+
+let loadClick = () => {
     loading(true)
     setTimeout(function () {
-        run(x)
+        run()
     }, 1000);
 }
 
-function run(presentName) {
-    runPresent = pyodideGlobals.get('runPresent')
-    proxy = runPresent(presentName)
+let togglePreset = (name) => {
+    let buttons = document.getElementsByClassName("preset")
+    let activateButton = document.getElementById("preset_" + name)
 
-    gammas = []
-    data = {}
+    // remove file from file input
+    let file = document.getElementById('file')
+    file.value = ""
+    let fileName = document.getElementById('filename')
+    fileName.innerHTML = ""
 
-    let gamma_proxy = proxy.get(0)
-    let data_proxy = proxy.get(1)
-
-    for (let i = 0; i < gamma_proxy.length; i++) {
-        gammas.push(gamma_proxy.get(i))
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = "#fff9c4"
     }
 
-    for (let i = 0; i < data_proxy.length; i++) {
-        let name = data_proxy.get(i).get(0)
+    let delimType = form.elements['delim']
+    let dateFormat = form.elements['date-format']
+    let riskFactor = form.elements['risk-factor']
+    let riskFree = form.elements['risk-free']
+    let dateRangeStart = form.elements['date-range-start']
+    let dateRangeEnd = form.elements['date-range-end']
 
-        data[name] = {
-            'name': name,
-            'src': [],
-            'sig': [],
-        }
+    delimType.value = "comma"
+    delimType.checked = true
 
-        srs = data_proxy.get(i).get(1)
-        sigs = data_proxy.get(i).get(2)
+    switch (name) {
+        
+        case "spsector":
+            riskFactor.value = "6"
+            dateFormat.value = "YYYYMMDD"
+            dateRangeStart.value = "19990101"
+            dateRangeEnd.value = "20201201"
+            break;
+        
+        case "industry":
+            riskFactor.value = "-1"
+            dateFormat.value = "YYYYMM"
+            dateRangeStart.value = "199901"
+            dateRangeEnd.value = "202012"
+            break;
 
-        for (let j = 0; j < srs.length; j++) {
-            data[name]['src'].push(srs.get(j))
-            data[name]['sig'].push(sigs.get(j))
-        }
+        case "international":
+            riskFactor.value = "-1"
+            dateFormat.value = "YYYYMM"
+            dateRangeStart.value = "199901"
+            dateRangeEnd.value = "202012"
+            break;
+        
+        case "25_1":
+            riskFactor.value = "-1"
+            riskFree.value = "0"
+            dateFormat.value = "YYYYMM"
+            dateRangeStart.value = "199901"
+            dateRangeEnd.value = "202012"
+            break;
+
+        case "25_3":
+            riskFactor.value = "-1, -2, -3"
+            riskFree.value = "0"
+            dateFormat.value = "YYYYMM"
+            dateRangeStart.value = "199901"
+            dateRangeEnd.value = "202012"
+            break;
+        
+        case "25_4":
+            riskFactor.value = "-1, -2, -3, -4"
+            riskFree.value = "0"
+            dateFormat.value = "YYYYMM"
+            dateRangeEnd.value = "202012"
+            break;
+    
+        case "ff4":
+            riskFactor.value = "0"
+            riskFree.value = "-1"
+            dateFormat.value = "YYYYMM"
+            dateRangeEnd.value = "202012"
+            break;
+
     }
 
-    showResults(gammas, data)
+    if (selectedPreset == "") {
+        activateButton.style.backgroundColor = "#fff176"
+        selectedPreset = name
+    } else if (selectedPreset == name) {
+        selectedPreset = ""
+        riskFactor.value = ""
+        riskFree.value = ""
+        dateFormat.value = ""
+    } else {
+        activateButton.style.backgroundColor = "#fff176"
+        selectedPreset = name
+    }
+}
+
+let openUploadDialogue = () => {
+    document.getElementById('file').click()
+
+    let buttons = document.getElementsByClassName("preset")
+    selectedPreset = ""
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = "#fff9c4"
+    }
+}
+
+let showFileName = () => {
+    let file = document.getElementById('file').files[0]
+    let fileName = document.getElementById('filename')
+    fileName.innerHTML = file.name
+}
+
+let run = (data) => {
+    // convert data to json string
+    let dataJson = JSON.stringify(data)
+    
+    runModel = pyodideGlobals.get('runModel')
+    proxy = runModel(dataJson)
+
+    // gammas = []
+    // data = {}
+
+    // let gamma_proxy = proxy.get(0)
+    // let data_proxy = proxy.get(1)
+
+    // for (let i = 0; i < gamma_proxy.length; i++) {
+    //     gammas.push(gamma_proxy.get(i))
+    // }
+
+    // for (let i = 0; i < data_proxy.length; i++) {
+    //     let name = data_proxy.get(i).get(0)
+
+    //     data[name] = {
+    //         'name': name,
+    //         'src': [],
+    //         'sig': [],
+    //     }
+
+    //     srs = data_proxy.get(i).get(1)
+    //     sigs = data_proxy.get(i).get(2)
+
+    //     for (let j = 0; j < srs.length; j++) {
+    //         data[name]['src'].push(srs.get(j))
+    //         data[name]['sig'].push(sigs.get(j))
+    //     }
+    // }
+
+    // showResults(gammas, data)
     loading(false);
 }
 
